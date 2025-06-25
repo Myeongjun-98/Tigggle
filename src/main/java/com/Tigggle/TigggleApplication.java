8e70b0bf666c1ad16e1429816c19758e56c7643c
Myeongjun-98 <anju304@naver.com>
2025-06-25 17:51:59 +0900

    sssssfd

---
+++ src/main/java/com/Tigggle/TigggleApplication.java
@@ -0,0 +1,13 @@
+package com.Tigggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigggleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigggleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tigggle/TigggleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tigggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigggleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
