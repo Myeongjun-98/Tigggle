5f83cb88da6623078b87be774efb4bc6f14d7c99
tlssunw <sunw1044@gmail.com>
2025-06-30 17:36:29 +0900

    6월 30일 병합하기 전 메모

--- src/main/java/com/Tigggle/Controller/insite/instieController.java
+++ src/main/java/com/Tigggle/Controller/insite/instieController.java
@@ -4,16 +4,22 @@ package com.Tigggle.Controller.insite;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class instieController {
 
-    @GetMapping("/")
+    @GetMapping("/insite")
     public String insite(){
-        return "/";
+        return "/insite";
     }
 
+    // 월간 소비내역
+    //public String
+    // 이번달 소비내역
+
     // 엑셀로 다운로드
-    public String
+
+
 }
