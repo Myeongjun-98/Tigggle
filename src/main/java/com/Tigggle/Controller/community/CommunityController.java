419d691b93b14d9cbaf9f899e14d1634b8f4c191
junghoon <ljh2ljh2@naver.com>
2025-07-01 11:39:30 +0900

    0701 11:39

--- src/main/java/com/Tigggle/Controller/community/CommunityController.java
+++ src/main/java/com/Tigggle/Controller/community/CommunityController.java
@@ -1,15 +1,29 @@
 package com.Tigggle.Controller.community;
 
+import com.Tigggle.Constant.CommunityCategory;
+import com.Tigggle.DTO.community.CommunityBoardListDto;
+import com.Tigggle.Service.community.CommunityBoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 
 public class CommunityController {
 
+    private final CommunityBoardService communityBoardService;
+
     @GetMapping("/communityTip")
 
-        public String communityTip() {
+    public String communityTip(Model model) {
+
+        List<CommunityBoardListDto> communityBoardListDtos = communityBoardService
+                .getCommunityBoards(CommunityCategory.TIP);
+        model.addAttribute("communityBoardListDtos", communityBoardListDtos);
 
         return "community/Tip";
 
@@ -17,15 +31,23 @@ public class CommunityController {
 
     @GetMapping("/communityDiscussion")
 
-        public String communityDiscussion() {
+    public String communityDiscussion(Model model) {
 
-            return "community/Discussion";
+        List<CommunityBoardListDto> communityBoardListDtos = communityBoardService
+                .getCommunityBoards(CommunityCategory.DISCUSSION);
+        model.addAttribute("communityBoardListDtos", communityBoardListDtos);
+
+        return "community/Discussion";
 
     }
 
     @GetMapping("/communityEconomicMarket")
 
-    public String communityEconomicMarket() {
+    public String communityEconomicMarket(Model model) {
+
+        List<CommunityBoardListDto> communityBoardListDtos = communityBoardService
+                .getCommunityBoards(CommunityCategory.ECONOMIC_MARKET);
+        model.addAttribute("communityBoardListDtos", communityBoardListDtos);
 
         return "community/EconomicMarket";
     }
