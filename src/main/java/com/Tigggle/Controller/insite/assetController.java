24be4843109024cf5b34eefa2f5f124191f2e3d5
tlssunw <sunw1044@gmail.com>
2025-07-03 10:52:09 +0900

    신선우 바보

---
+++ src/main/java/com/Tigggle/Controller/insite/assetController.java
@@ -0,0 +1,13 @@
+package com.Tigggle.Controller.insite;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class assetController {
+
+        @GetMapping("")
+        public String asset(){
+            return "";
+        }
+}
+
