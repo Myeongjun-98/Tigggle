b7a57e8480846fdf09e30e9f4bd7131f9d7349ac
tlssunw <sunw1044@gmail.com>
2025-07-03 10:43:41 +0900

    신선우 바보

--- src/main/java/com/Tigggle/Controller/insite/AssetController.java
+++ src/main/java/com/Tigggle/Controller/insite/AssetController.java
@@ -1,15 +1,14 @@
 package com.Tigggle.Controller.insite;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequiredArgsConstructor
-public class AssetController {
+public class assetController {
 
     @GetMapping("")
     public String asset(){
         return "";
     }
+
 }
