18740b8088aa310d2bc42b4de06099df02d6e15b
junghoon <ljh2ljh2@naver.com>
2025-07-11 19:18:37 +0900

    0711 19:18

--- src/main/java/com/Tigggle/Config/AppConfig.java
+++ src/main/java/com/Tigggle/Config/AppConfig.java
@@ -1,5 +1,11 @@
 package com.Tigggle.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class AppConfig {
+
 
 }
